79dbbf79cd67beee4ff60f02b952bfd81e564505
Lucasss2612 <lucasss2612@gmail.com>
2024-11-10 14:55:30 -0500

    probando backend

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,16 +1,16 @@
-const Game = require('../models/game');
+import Game, { find } from '../models/game';
 
 // Obtener todos los videojuegos
-exports.getGames = async (req, res) => {
+export async function getGames(req, res) {
   try {
-    const games = await Game.find();
+    const games = await find();
     res.status(200).json(games);
   } catch (error) {
     res.status(500).json({ error: 'Error al obtener videojuegos' });
   }
-};
+}
 // Crear un nuevo videojuego
-exports.createGame = async (req, res) => {
+export async function createGame(req, res) {
   const { name, category, price, description, developer, systemRequirements } = req.body;
   const newGame = new Game({ name, category, price, description, developer, systemRequirements });
   
@@ -20,4 +20,4 @@ exports.createGame = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'Error al crear el videojuego' });
   }
-};
+}
